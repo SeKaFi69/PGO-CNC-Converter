1a9fefc0a5c746329acff3731a3c64af4d2f9ae6
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2023-06-29 16:21:00 +0200

    v1.0.3

---
+++ SourceCode/functions.py
@@ -0,0 +1,6 @@
+import os
+hass =      ["G43 G58", "G71", "M30"]
+axa =       ["G54 G64", "G71", "M02"]
+feller =    ["G43 G54", "G21", "M02"]
+names = ["FELLER", "AXA", "HASS"]
+types = [feller, axa, hass]
